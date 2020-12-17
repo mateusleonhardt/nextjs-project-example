953faa91f7beda20d8b1f6d447e0df6e8a67e2bb
Mateus Leonhardt <mateus.leonhardt@gmail.com>
2020-12-17 16:13:20 -0300

    Applying SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from 'next';
 import { Title } from '@/styles/pages/Home';
+import SEO from '@/components/SEO';
 
 interface IProduct {
   id: string;
@@ -13,6 +14,10 @@ interface HomeProps {
 export default function Home({ recommendedProducts }: HomeProps) {
   return (
     <div>
+      <SEO 
+        title="DevCommerce, your best e-commerce!" 
+        shouldExcludeTitleSuffix
+      />
       <section>
         <Title>Products</Title>
 
