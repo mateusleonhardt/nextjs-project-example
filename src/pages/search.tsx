5bb46fe0ee27884b85e887e4a8a7091e19a44fda
Mateus Leonhardt <mateus.leonhardt@gmail.com>
2020-12-18 15:21:42 -0300

    Search page

---
+++ src/pages/search.tsx
@@ -0,0 +1,67 @@
+import { GetServerSideProps } from "next";
+import Link from 'next/link';
+import { useRouter }from "next/router";
+import { FormEvent, useState } from "react";
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
+import { client } from "@/lib/prismic";
+
+interface SearchProps {
+    searchResults: Document[];
+}
+
+export default function Search({ searchResults }: SearchProps) {
+    const router = useRouter();
+    const [search, setSearch] = useState('');
+
+    function handleSearch(e: FormEvent) {
+        e.preventDefault();
+
+        router.push(
+            `/search?q=${encodeURI(search)}`
+        )
+
+        setSearch('');
+
+    }
+    return (
+        <div>
+            <form onSubmit={handleSearch}>
+                <input type="text" value={search} onChange={e => setSearch(e.target.value)} />
+                <button type="submit">Search</button>
+            </form>
+
+            <ul>
+                {searchResults.map(product =>{
+                    return (
+                    <li key={product.id}>
+                        <Link href={`/catalogs/products/${product.uid}`}>
+                            {PrismicDOM.RichText.asText(product.data.title)}            
+                        </Link>
+                    </li>
+                    );
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps<SearchProps> = async (context) => {
+    const { q } = context.query;
+
+    if (!q) {
+        return { props: { searchResults: [] } };
+    }
+
+    const searchResults = await client().query([
+        Prismic.Predicates.at('document.type', 'product'),
+        Prismic.Predicates.fulltext('my.product.title', String(q))
+    ])
+
+    return {
+        props: {
+            searchResults: searchResults.results
+        }
+    }
+};
